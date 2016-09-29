73d8d6b2410e5a6b79e3f1482ccdf8ef54f30111
Collin Cromwell <collcrom@gmail.com>
2016-09-29 17:01:55 -0400

    p3 redo

---
+++ P3/P3/p3.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+struct Node
+{
+	T info;
+	Node<T> *link;
+};
+
+template <typename T>
+class LinkedList
+{
+public:
+	LinkedList();
+	~LinkedList();
+	void print() const;
+	void insert(const T& item);
+protected:
+	int count;
+	Node<T> *first;
+};
+void main()
+{
+	LinkedList<char> c;
+	c.insert('D');
+	c.insert('B');
+	c.insert('A');
+	c.insert('Z');
+	c.insert('S');
+	c.insert('Z');
+	c.insert('C');
+	c.print();
+	return;
+}
+
+template<typename T>
+LinkedList<T>::LinkedList()
+{
+	first = NULL;
+}
+
+template<typename T>
+LinkedList<T>::~LinkedList()
+{
+}
+
+template<typename T>
+void LinkedList<T>::print() const
+{
+	while (first != NULL)
+	{
+		cout << first->info << endl;
+	}
+}
+
+template<typename T>
+void LinkedList<T>::insert(const T & item)
+{
+	Node<T> *temp = new Node<T>();
+	temp->info = item;
+	temp->link = NULL;
+	first = temp;
+}
